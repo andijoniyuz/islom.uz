74f239b4b2fe81ebd439a8554f801874520c2db7
andijoniyuz <83279170+andijoniyuz@users.noreply.github.com>
2021-04-27 17:29:31 +0500

    Update urls.py

--- tilon/urls.py
+++ tilon/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import NamazView, SearchQuestView, QuestView, HomeView
 
 urlpatterns = [
-    path('', HomeView(), name='Home'),
+    path('/', HomeView(), name='Home'),
     path('namoz/', NamazView, name='namaz'),
     path('savol/', SearchQuestView, name='savol'),
     path('savol/<int:quest_id>', QuestView, name='savol'),
