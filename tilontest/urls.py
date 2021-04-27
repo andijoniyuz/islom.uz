7e6c47b860b6cb65d66303c286505e96bea3131d
andijoniyuz <83279170+andijoniyuz@users.noreply.github.com>
2021-04-27 18:22:54 +0500

    Update urls.py

--- tilontest/urls.py
+++ tilontest/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from tilon.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tilon.urls')),
-    path('', HomeView)
+    path('', include('apis.urls'))
 ]
